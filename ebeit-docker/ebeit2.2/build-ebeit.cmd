8856379c6e79511655f661883bed0909e3607243
sunchey11 <sunchey11@users.noreply.github.com>
2023-12-07 17:24:39 +0800

    整理ebeit docker

--- ebeit-docker/ebeit2.2/build-ebeit.cmd
+++ ebeit-docker/ebeit2.2/build-ebeit.cmd
@@ -3,3 +3,6 @@ docker build -t ebeit-docker .
 docker run --network ebeit-net --name ebeit_demo --rm -it -p 2279:2279 ebeit-docker /bin/sh 
 
 docker run -it --network ebeit-net nicolaka/netshoot
+
+docker tag ebeit-docker anderssun/ebeit-docker
+docker push anderssun/ebeit-docker
